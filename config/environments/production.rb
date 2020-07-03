eb7526d28d914908ad17dec02fc374ec5b0ec222
Joseph Dudley <joseph@wearecast.org.uk>
2020-07-03 17:42:38 +0100

    Added missing ActionMailer config setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'catalyst-support.heroku.com' }
+
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
